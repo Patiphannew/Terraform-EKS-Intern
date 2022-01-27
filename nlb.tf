da4eeffcce0dc2313a2764e287c2257b460bbcfe
Patiphan Thiansuwan <newnismokung_@hotmail.com>
2022-01-27 10:17:22 +0700

    first commit

---
+++ nlb.tf
@@ -0,0 +1,35 @@
+module "nlb" {
+  source  = "terraform-aws-modules/alb/aws"
+  version = "~> 6.0"
+
+  name = "my-nlb"
+
+  load_balancer_type = "network"
+
+  vpc_id  = "vpc-b8d13ade"
+  subnets = ["subnet-977ea2f1", "subnet-6221ed2a", "subnet-7e65ee27"]
+
+  # access_logs = {
+  #   bucket = "new-bucket-test-test-test"
+  # }
+
+  target_groups = [
+    {
+      name_prefix      = "dd"
+      backend_protocol = "TCP"
+      backend_port     = 80
+      target_type      = "ip"
+    }
+  ]
+
+
+  http_tcp_listeners = [
+    {
+      port               = 80
+      protocol           = "TCP"
+      target_group_index = 0
+    }
+  ]
+
+
+}
---
+++ provider.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+   profile = "produser"
+   region = "ap-southeast-1"
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+  backend "s3" {
+    bucket = "new-myawsbucket"
+    key    = "./terraform.tfstate"
+    region = "ap-southeast-1" 
+   #  # Enable State Locking
+   #  dynamodb_table = "nopnithi-terraform-state-demo"
+    profile = "produser"
+  }
+}
